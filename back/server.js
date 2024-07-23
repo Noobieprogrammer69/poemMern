f4676ca8a09f7a45f2c00e5469be0416cb869c62
Jover <joververgara1@gmail.com>
2024-07-23 20:23:02 +0800

    deployment

--- back/server.js
+++ back/server.js
@@ -37,9 +37,9 @@ app.get('/', (req, res) => {
 })
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../front/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../front/dist", "index.html"));
     });
 }
 
